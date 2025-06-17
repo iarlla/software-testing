921a1e0389ac9b226717454c027fc542834f5bb6
Iarla Soares <97488674+iarlla@users.noreply.github.com>
2025-06-17 16:05:47 -0300

    Create TestData.py

---
+++ robotframework-automation/Variables/TestData.py
@@ -0,0 +1,11 @@
+VALID_USERNAME = "standard_user"
+VALID_PASSWORD = "secret_sauce"
+LOCKED_OUT_USERNAME = "locked_out_user"
+INVALID_USERNAME = "invalid_user"
+INVALID_PASSWORD = "invalid_password"
+
+FIRST_NAME = "Teste"
+LAST_NAME = "Pratico"
+ZIP_CODE = "12345"
+
+PRODUCTS = ["Sauce Labs Backpack", "Sauce Labs Bike Light", "Sauce Labs Bolt T-Shirt"]
