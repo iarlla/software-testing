781ce154c4af03e22231a2b55d01e76b923794b8
Iarla Soares <97488674+iarlla@users.noreply.github.com>
2025-06-17 16:07:29 -0300

    Create e2e.js

---
+++ cypress-automation/cypress/support/e2e.js
@@ -0,0 +1,11 @@
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively, you can use CommonJS syntax:
+// require('./commands')
+
+// Import Page Objects
+import './pages/LoginPage'
+import './pages/ProductsPage'
+import './pages/CartPage'
+import './pages/CheckoutPage'
